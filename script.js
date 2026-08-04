2f24b5bbaf02a8717fe6d19ce8acd5a3735a37c2
jharkhandvision123 <wingsgroup2026@gmail.com>
2026-08-04 11:54:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+const searchBox = document.getElementById("searchBox");
+const searchBtn = document.getElementById("searchBtn");
+
+if(searchBtn){
+
+searchBtn.addEventListener("click",()=>{
+
+const text = searchBox.value.trim();
+
+if(text===""){
+
+alert("Please enter something to search.");
+
+}else{
+
+alert("Searching for: " + text);
+
+}
+
+});
+
+}
+
 console.log("JharkhandVision123 Student Portal Loaded");
 
 });
