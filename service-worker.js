f0a29d45714473281c88dd64c63cc8937fb81b64
jharkhandvision123 <wingsgroup2026@gmail.com>
2026-08-12 00:19:56 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,7 @@
-```javascript id="m7q4vx"
-const CACHE_NAME = "student-portal-v1";
-
-self.addEventListener("install", function (event) {
-    console.log("Service Worker: Installing");
+self.addEventListener("install", event => {
     self.skipWaiting();
 });
 
-self.addEventListener("activate", function (event) {
-    console.log("Service Worker: Activated");
+self.addEventListener("activate", event => {
     event.waitUntil(self.clients.claim());
 });
-
-self.addEventListener("fetch", function (event) {
-    event.respondWith(
-        fetch(event.request).catch(function () {
-            return caches.match(event.request);
-        })
-    );
-});
-```
