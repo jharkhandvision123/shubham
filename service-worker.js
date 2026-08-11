f5bcb7266b89cc353f62e1dd428605992fbc8608
jharkhandvision123 <wingsgroup2026@gmail.com>
2026-08-12 00:16:27 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,21 @@
-```javascript id="7xq2lm"
+```javascript id="m7q4vx"
 const CACHE_NAME = "student-portal-v1";
 
-const FILES_TO_CACHE = [
-    "./",
-    "./index.html",
-    "./style.css",
-    "./script.js",
-    "./manifest.json"
-];
-
-self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(FILES_TO_CACHE))
-    );
-
+self.addEventListener("install", function (event) {
+    console.log("Service Worker: Installing");
     self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
-    event.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(
-                keys
-                    .filter(key => key !== CACHE_NAME)
-                    .map(key => caches.delete(key))
-            )
-        )
-    );
-
-    self.clients.claim();
+self.addEventListener("activate", function (event) {
+    console.log("Service Worker: Activated");
+    event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", function (event) {
     event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                return response || fetch(event.request);
-            })
+        fetch(event.request).catch(function () {
+            return caches.match(event.request);
+        })
     );
 });
 ```
